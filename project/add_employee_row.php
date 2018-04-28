99b85996691c583bb0878a52b268233294fe797d
jjsulzer <37268957+jjsulzer@users.noreply.github.com>
2018-04-27 22:30:27 -0500

    Added css

--- project/add_employee_row.php
+++ project/add_employee_row.php
@@ -1,14 +1,29 @@
 <html>
   
+<head>
+	<link rel="stylesheet" href= "style.css">
+</head>
+  
 <body>
+<img class ="employ_img" src = "img2.jpg" alt="Building Background">
 
 <form action="employee_info.php" method="post">
-ID <input type="text" name="id"><br />
-Name <input type="text" name="pname"><br />
-Commission (numbers only, no punctuation) <input type="text" name="comm"><br />
-Hire Date <input type="text" name="hire"><br />
+
+<div class = "commission"> 
+  
+	<div class ="nested">
+		ID: <input type="text" name="id"><br />
+		<br />
+		Name: <input type="text" name="pname"><br />
+		<br />
+		Commission:<input type="text" name="comm"><br />
+		<br />
+		Hire Date:<input type="text" name="hire"><br />
+	</div
 
 <input type="submit">
+
+</div>
 
 </form>
 
