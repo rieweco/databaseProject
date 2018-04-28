67e7a4f785f45657b4e761ef455026309a5f6283
jjsulzer <37268957+jjsulzer@users.noreply.github.com>
2018-04-27 23:59:06 -0500

    Added css

--- project/add_sku_row.php
+++ project/add_sku_row.php
@@ -1,16 +1,27 @@
 <html>
-  
+
+ <head>
+	<link rel="stylesheet" href="style.css">
+</head>
+
 <body>
 
+<img class ="equip_img" src = "img3.jpg" alt="merchandise">
+
 <form action="sku_info.php" method="post">
-SKU ID <input type="text" name="sku"><br />
-Cost Per Unit <input type="text" name="cost"><br />
-Quantity <input type="text" name="quantity"><br />
 
-<input type="submit">
-
+	<div class ="sku_info">
+		<div class = nested>
+			SKU ID: <input type="text" name="sku"><br />
+			<br />
+			Cost Per Unit: <input type="text" name="cost"><br />
+			<br />
+			Quantity: <input type="text" name="quantity"><br />
+			<br />
+			<input type="submit">
+		</div>
+	</div>
 </form>
-
 
 </body>
 
