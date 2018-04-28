3db27f7acd08f593f282b598e8dab57e477265f9
jjsulzer <37268957+jjsulzer@users.noreply.github.com>
2018-04-27 23:58:33 -0500

    Added css

--- project/add_sales_row.php
+++ project/add_sales_row.php
@@ -1,20 +1,38 @@
 <html>
-  
+
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+
 <body>
 
+<div class = "top"> 
+</div> 
+
 <form action="sales_info.php" method="post">
-Sale ID <input type="text" name="sale_id"><br />
-Employee ID <input type="text" name="emp_id"><br />
-SKU ID <input type="text" name="sku_id"><br />
-Net Profit <input type="text" name="profit"><br />
-Sale Date <input type="text" name="sale_date"><br />
-Quantity <input type="text" name="quantity"><br />
-Notes <input type="text" name="notes"><br />
 
-<input type="submit">
-
+	<div class = "sales_info">
+		Sale ID: <input type="text" name="sale_id"><br />
+		<br />
+		Employee ID: <input type="text" name="emp_id"><br />
+		<br />
+		SKU ID: <input type="text" name="sku_id"><br />
+		<br />
+		Net Profit: <input type="text" name="profit"><br />
+		<br />
+		Sale Date: <input type="text" name="sale_date"><br />
+		<br />
+		Quantity: <input type="text" name="quantity"><br />
+		<br />
+		Notes: <input type="text" name="notes"><br />
+		<br />
+		<br />
+		<input type="submit">
+	</div>
 </form>
 
+<div class = "bottom"> 
+</div> 
 
 </body>
 
