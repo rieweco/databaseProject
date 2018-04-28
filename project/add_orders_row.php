cd0efeb5b1d9c2a06977db8bc78679549890e9e6
jjsulzer <37268957+jjsulzer@users.noreply.github.com>
2018-04-27 23:57:08 -0500

    Added CSS

--- project/add_orders_row.php
+++ project/add_orders_row.php
@@ -1,21 +1,31 @@
 <html>
   
+ <head>
+	<link rel="stylesheet" href="style.css">
+</head>
+  
 <body>
 
 <form action="orders_info.php" method="post">
-Order ID <input type="text" name="id"><br />
-SKU ID <input type="text" name="sku_id"><br />
-Vendor Name <input type="text" name="pname"><br />
-Order Date <input type="text" name="order_date"><br />
-Arrival Date <input type="text" name="arrival_date"><br />
-Order Amount <input type="text" name="amount"><br />
-Shipping Cost <input type="text" name="cost"><br />
 
-<input type="submit">
-
+	<div class = "orders">
+		Order ID: <input type="text" name="id"><br />
+		<br />
+		SKU ID: <input type="text" name="sku_id"><br />
+		<br />
+		Vendor Name: <input type="text" name="pname"><br />
+		<br />
+		Order Date: <input type="text" name="order_date"><br />
+		<br />
+		Arrival Date: <input type="text" name="arrival_date"><br />
+		<br />
+		Order Amount: <input type="text" name="amount"><br />
+		<br />
+		Shipping Cost: <input type="text" name="cost"><br />
+		</br>
+		<input type="submit">
+	</div> 
 </form>
-
-
 </body>
 
 </html>
