fd2c04607095aad103fca57de6efbc710e1e3fbb
jjsulzer <37268957+jjsulzer@users.noreply.github.com>
2018-04-29 16:03:25 -0500

    Added css

--- project/add_products_row.php
+++ project/add_products_row.php
@@ -1,17 +1,33 @@
 <html>
-  
+ 
+ <head>
+	<link rel="stylesheet" href="style.css">
+</head>
+ 
 <body>
 
+<div class = "top"> 
+<h1>Dunder Mifflin</h1>
+</div> 
+
 <form action="employee_info.php" method="post">
-ID <input type="text" name="id"><br />
-Name <input type="text" name="pname"><br />
-Commission (numbers only, no punctuation) <input type="text" name="comm"><br />
-Hire Date <input type="text" name="hire"><br />
 
-<input type="submit">
-
+	<div class = "add_products">
+		ID: <input type="text" name="id" placeholder="Employee number"><br />
+		<br />
+		Name: <input type="text" name="pname" placeholder="Product name"><br />
+		<br />
+		Commission: <input type="text" name="comm" placeholder="Numbers only"><br />
+		<br />
+		Hire Date: <input type="text" name="hire" placeholder="xx/xx/xxxx"><br />
+		<br />
+		<input type="submit">
+	</div> 
+	
 </form>
 
+<div class = "bottom"> 
+</div> 
 
 </body>
 
